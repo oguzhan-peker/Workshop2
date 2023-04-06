0b3af1e7e067fcb6095c8aae2268fe07f87c0583
oguzhan-peker <peker.oguzhan1634@gmail.com>
2023-04-06 20:37:08 +0300

    day2

---
+++ abstractClasses/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        GameCalculator gameCalculator=new GameCalculator() {
+            @Override
+            public void hesapla() {
+
+            }
+        };
+    }
+}
